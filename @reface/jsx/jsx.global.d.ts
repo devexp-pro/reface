3c1b05cabce983fdf0d449e7dea9a42e396ad927
Sergey Andreev <DragorWW@gmail.com>
2024-12-23 11:50:37 +0700

    types: jsx

--- @reface/jsx/jsx.global.d.ts
+++ @reface/jsx/jsx.global.d.ts
@@ -1,29 +1,30 @@
 import type {
-  ComponentProps,
-  ComponentWithProps,
   ElementChildType,
-  IRefaceTemplate,
-} from "@reface/types";
+  Template,
+  TemplateAttributes,
+} from "@reface/template";
 
 declare global {
   namespace JSX {
     // Базовый тип для JSX элементов
-    type Element = IRefaceTemplate;
+    type Element = Template;
 
     // Определяем как компоненты получают children
     interface ElementChildrenAttribute {
-      children: ElementChildType | ElementChildType[];
+      children: ElementChildType;
     }
 
     // Определяем встроенные HTML элементы
     interface IntrinsicElements {
-      [elemName: string]: ComponentProps;
+      [elemName: string]: TemplateAttributes;
     }
 
-    // Определяем тип компонента - функция, которая может быть использована как JSX компонент
-    type ElementType = string | ComponentWithProps;
+    // Определяем тип компонента
+    type ElementType =
+      | string
+      | ((props: any, children?: ElementChildType) => Template);
 
     // Определяем атрибуты для пользовательских компонентов
-    interface IntrinsicAttributes extends ComponentProps {}
+    interface IntrinsicAttributes extends TemplateAttributes {}
   }
 }
--- @reface/template/mod.ts
+++ @reface/template/mod.ts
@@ -1,5 +1,6 @@
 export type {
   ComponentFn,
+  ElementChildType,
   RawTemplate,
   RawTemplateAttributes,
   Template,
